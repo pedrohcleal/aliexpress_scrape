91d545451549c537cd03fd2704ef441c72586ed9
Pedro Leal <59542610+pedrohcleal@users.noreply.github.com>
2024-09-05 20:23:10 -0300

    Update chrome_config.py

--- chrome_config.py
+++ chrome_config.py
@@ -4,7 +4,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 
 chrome_options = Options()
-# chrome_options.add_argument("--headless") # modo sem janela, apenas terminal
+chrome_options.add_argument("--headless") # modo sem janela, apenas terminal
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--disable-notifications")
