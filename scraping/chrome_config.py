0d62f86a2e5deac587d3a314be7bad477eaa2348
Pedro Leal <59542610+pedrohcleal@users.noreply.github.com>
2024-09-06 21:44:08 -0300

    chrome optios

--- scraping/chrome_config.py
+++ scraping/chrome_config.py
@@ -5,13 +5,13 @@ from selenium import webdriver
 
 def get_driver():
     chrome_options = Options()
-    #chrome_options.add_argument("--headless") # modo sem janela, apenas terminal
+    chrome_options.add_argument("--headless") # modo sem janela, apenas terminal
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-notifications")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument("--window-size=1920,1080")  # tamanho da guia do chrome
+    #chrome_options.add_argument("--window-size=1920,1080")  # tamanho da guia do chrome
     chrome_options.add_argument("--disable-animations")
     chrome_options.add_argument("--disable-cache")
     chrome_options.add_argument("--disable-prefetch")
