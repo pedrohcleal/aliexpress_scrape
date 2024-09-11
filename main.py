9ff0665b98d6c754c61f28a44d45c7d09db3e34e
Pedro Leal <59542610+pedrohcleal@users.noreply.github.com>
2024-09-11 13:39:52 -0300

    final update

--- main.py
+++ main.py
@@ -17,42 +17,6 @@ if __name__ == "__main__":
     sleep(1)
     print(f'Json lido = {path_json}')
     try:
-        final_json = []
-        for json_dict in json_load:
-            contador += 1
-            url = json_dict["link"]
-            dados_item = {
-                "idProduct": json_dict["idProduct"],
-                "id_ali": get_ali_id(url),
-                "link_ali": url,
-                "oldPrice": json_dict["oldPrice"],
-                "oldStock": json_dict["oldStock"]
-            }
-
-            print(f'Carregando página, URL = {json_dict["link"]}')
-            driver.get(url)
-            sleep(random.randrange(10, 20))  # ajustar tempo de mundança de link -> randrange(10,20) -> escolhe tempo aleatorio entre 10 a 20segundos
-
-            titulo = driver.find_element(
-                By.CSS_SELECTOR,
-                "#root > div > div.pdp-body.pdp-wrap > div > div.pdp-body-top-left > div.pdp-info > div.pdp-info-right > div.title--wrap--UUHae_g > h1",
-            )
-            dados_item["ali_nome_produto"] = titulo.text
-
-            max_wait_time = 1
-            dados_item["frete"] = texto_frete(driver, max_wait_time, tratar_texto_frete)
-            dados_item["newPrice"] = preco_produto(driver, max_wait_time)
-
-            if e_primeira_compra(driver, max_wait_time):
-                dados_item["e_primeira_compra"] = True
-            else:
-                dados_item["e_primeira_compra"] = False
-
-            if verificar_estoque(driver, max_wait_time):
-                dados_item["em_estoque"] = True
-            else:
-                dados_item["em_estoque"] = False
-
             final_json.append(dados_item)
             print('Produto lido:')
             pprint.pprint(dados_item)
